3e5d8481b262557d576bb9dedd6421f64b56bf7a
biancagante <biancaagantetiene@gmail.com>
2026-09-03 17:27:46 -0300

    feat - app DAO de calculo de IMC

---
+++ AppIMC/app/src/main/java/br/edu/fatecpg/appimc/view/IMCActivity.kt
@@ -0,0 +1,21 @@
+package br.edu.fatecpg.appimc.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.fatecpg.appimc.R
+
+class IMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imcactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
